e856f684fbbd45db90c17c8af752fc913d6dbc01
Nele Quast <nele.quast@dtc.ox.ac.uk>
2021-10-29 09:32:46 +0100

    #14 fixed flake8 lint issues

--- Molecule.py
+++ Molecule.py
@@ -5,7 +5,8 @@ from rdkit.Chem import Descriptors
 from rdkit.Chem import rdMolDescriptors
 from rdkit.Chem.Draw import rdMolDraw2D
 from rdkit.Chem import Crippen
-from rdkit.Chem.FilterCatalog import *
+from rdkit.Chem import FilterCatalog
+from rdkit.Chem.FilterCatalog import FilterCatalogParams
 from rdkit.Chem import AllChem
 
 
@@ -15,8 +16,10 @@ csv_file = pd.read_csv('r_group_decomp.csv')
 
 
 class Molecule:
-    """A molecule. In particular, either the R1 or R2 group, or the scaffold and one or two groups.
-     There are methods which tell you the properties of the molecule and if it passes the Lipsinki test."""
+    """A molecule. In particular, either the R1 or R2 group, or the scaffold
+    and one or two groups.
+     There are methods which tell you the properties of the molecule and if it
+     passes the Lipsinki test."""
 
     def __init__(self, mol_smiles):
         self.mol_smiles = mol_smiles
@@ -33,38 +36,41 @@ class Molecule:
         rings = Lipinski.RingCount(mol)
 
         desc_dict = {'mol': self.mol_smiles,
-                    'MW': mw,
-                    'logP': log_p,
-                    'TPSA': tpsa,
-                    'HA': ha,
-                    'h_acc': h_acceptors,
-                    'h_don': h_donors,
-                    'rings': rings
-                    }
+                     'MW': mw,
+                     'logP': log_p,
+                     'TPSA': tpsa,
+                     'HA': ha,
+                     'h_acc': h_acceptors,
+                     'h_don': h_donors,
+                     'rings': rings
+                     }
         return desc_dict
 
     def lipinski(self, desc_dict):
         """Calculate Lipinski from the descriptor dictionary.
         Return the number of rules broken and whether the molecule passes.
         """
-        violations=[desc_dict['MW'] >= 500.0, desc_dict['h_acc'] >= 10, 
-        desc_dict['h_don'] >= 5, desc_dict['logP'] >= 5].count(True)
+        violations = [desc_dict['MW'] >= 500.0,
+                      desc_dict['h_acc'] >= 10,
+                      desc_dict['h_don'] >= 5,
+                      desc_dict['logP'] >= 5
+                      ].count(True)
         if violations > 1:
             result = 'fails'
         else:
             result = 'passes'
-        
         return violations, result
 
     def draw_molecule(self, drawn_file_name, orient_with_scaffold):
-        """Draws the molecule. The variable 'orient_with_scaffold' can take 
-        the values True and False. It should take the value True if and 
+        """Draws the molecule. The variable 'orient_with_scaffold' can take
+        the values True and False. It should take the value True if and
         only if the molecule contains the scaffold"""
         drawn_mol = Chem.MolFromSmiles(self.mol_smiles)
-        #Align molecule with scaffold if the molecule contains the scaffold.
-        if orient_with_scaffold == True:
+        # Align molecule with scaffold if the molecule contains the scaffold.
+        if orient_with_scaffold is True:
             AllChem.Compute2DCoords(scaffold)
-            _ =AllChem.GenerateDepictionMatching2DStructure(drawn_mol, scaffold)
+            _ = AllChem.GenerateDepictionMatching2DStructure(drawn_mol,
+                                                             scaffold)
         d = rdMolDraw2D.MolDraw2DCairo(250, 200)
         d.drawOptions().addStereoAnnotation = True
         d.drawOptions().clearBackground = False
@@ -72,7 +78,7 @@ class Molecule:
         d.FinishDrawing()
         d.WriteDrawingText(f'{drawn_file_name}.png')
 
-    def filter_properties(self):
+    def filter_properties(self, drawn_mol_final):
         """See whether molecule passes or fails FILTERS"""
         params = FilterCatalogParams()
         params.AddCatalog(FilterCatalogParams.FilterCatalogs.PAINS_A)
@@ -88,23 +94,26 @@ class Molecule:
             print("PASSED FILTERS")
 
 
-class R_group(Molecule): 
-    """Name of R group is of the form 'Axy' or 'Bxy' e.g. A01 etc. 
+class R_group(Molecule):
+    """Name of R group is of the form 'Axy' or 'Bxy' e.g. A01 etc.
     Number corresponds to whether it is an R1 or R2 group"""
     def ___init___(self, name, number):
-        self.name=name
-        self.number=number
+        self.name = name
+        self.number = number
 
     def extract_smilefromcsv(self):
         """Extracts the SMILE for the R group """
         if self.number == 1:
-            rgroup_smiles = csv_file[csv_file['atag']==self.name]['R1'][0]
+            rgroup_smiles = csv_file[csv_file['atag'] == self.name]['R1'][0]
         if self.number == 2:
-            rgroup_smiles = csv_file[csv_file['btag']==self.name]['R2'][0]
+            rgroup_smiles = csv_file[csv_file['btag'] == self.name]['R2'][0]
         print(rgroup_smiles)
 
-class Scaffold_and_Rgroups(Molecule): #Can I also make this have R-group???
-    """Add a R group to the old molecule (either the scaffold with R1 attached or just the scaffold)."""
+
+class Scaffold_and_Rgroups(Molecule):
+    # Can I also make this have R-group???
+    """Add a R group to the old molecule (either the scaffold with R1 attached
+    or just the scaffold)."""
     def ___init___(self, old_molecule, rgroup, number):
         self.old_molecule = old_molecule
         self.rgroup = rgroup
@@ -118,12 +127,14 @@ class Scaffold_and_Rgroups(Molecule): #Can I also make this have R-group???
         new_mol = new_mol.replace('(9)', '9')
         self.new_molecule = new_mol
 
+
 class FinalMolecule(Molecule):
     """Final molecule with scaffold and two R groups."""
-    def ___init___(self, rgroup1, rgroup2):     #Name of R groups should be in the form 'Axy' or 'Bxy' e.g. A01 etc. 
+    def ___init___(self, rgroup1, rgroup2):
+        # Name of R groups should be in the form 'Axy' or 'Bxy' e.g. A01 etc.
         self.rgroup1 = rgroup1
         self.rgroup2 = rgroup2
-    
+
     def drug_properties(self):
         """Selects properties of the final drug from the data."""
         drug_properties = [
@@ -134,6 +145,6 @@ class FinalMolecule(Molecule):
                             'pampa'
                             ]
         for d in drug_properties:
-            value = csv_file[csv_file['atag']==self.rgroup1]
-            value_two = value[csv_file['btag']==self.rgroup2][d][0]
+            value = csv_file[csv_file['atag'] == self.rgroup1]
+            value_two = value[csv_file['btag'] == self.rgroup2][d][0]
         print(d, value_two)
