84f1d004db84327babd43b233b9b99860091071a
k-shep <92580678+k-shep@users.noreply.github.com>
2021-12-08 17:06:35 +0000

    44 CHange conf.py file so same as in other project

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,10 @@ author = 'Nele Quast, Katherine Shepherd, Guy Durant, Dylan Adlard, Olivia'
 'Simpson, Stephanie Wills'
 
 
+# The full version, including alpha/beta/rc tags
+# release = '0.1.0'
+
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -40,7 +44,8 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
+# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -48,9 +53,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+# html_static_path = []
