33970905a543a4075284cefdf48bdd85283e1cc9
k-shep <92580678+k-shep@users.noreply.github.com>
2021-12-08 17:19:51 +0000

    Trying different file path in conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,9 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
-sys.path.insert(0, os.path.abspath('../../tests'))
+# sys.path.insert(0, os.path.abspath('../../src'))
+# sys.path.insert(0, os.path.abspath('../../tests'))
+sys.path.insert(0, os.path.abspath('../../drug-discovery-game-backend'))
 
 # -- Project information -----------------------------------------------------
 
