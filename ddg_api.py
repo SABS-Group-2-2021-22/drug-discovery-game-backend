c41e8a366a5d58d2c2aa80e5731f6d8c9c6ff9ec
guidodee <guydurant@btinternet.com>
2022-03-09 16:50:30 +0000

    Initial version of the results page api calls for the sketcher version of the game

--- ddg_api.py
+++ ddg_api.py
@@ -1,10 +1,10 @@
-from argparse import ArgumentError
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from src.Molecule import R_group, Molecule, FinalMolecule
 from src.sketchedMolecule import sketchedMolecule
 import base64
+import rdkit
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*":
@@ -16,6 +16,9 @@ molecule_info = {}
 
 global chosen_mol
 chosen_mol = [None, None]
+
+global sketched_chosen_mol
+sketched_chosen_mol = []
 
 global money
 money = [100000.0]
@@ -53,19 +56,106 @@ def sketcher_save_molecule():
     :rtypes: json dict
     '''
     mol_block = base64.b64decode(request.args.get('mol'))
-    # try:
-    new_mol = sketchedMolecule(mol_block)
-    return jsonify({'smiles': new_mol.smiles,
-                    'img_html': new_mol.drawMoleculeAsByteStream(),
-                    'descriptors': new_mol.descriptors(),
-                    'filters': new_mol.filter_properties(),
-                    'lipinski': new_mol.lipinski(),
-                    'assays_run': {},
-                    'tanimoto': new_mol.get_tanimoto_similarity(),
-                    'assays': new_mol.drug_properties(),
-                    })
-    # except:
-    #     return jsonify('failure')
+    try:
+        new_mol = sketchedMolecule(mol_block)
+        return jsonify({'smiles': new_mol.smiles,
+                        'img_html': new_mol.drawMoleculeAsByteStream(),
+                        'descriptors': new_mol.descriptors(),
+                        'filters': new_mol.filter_properties(),
+                        'lipinski': new_mol.lipinski(),
+                        'assays_run': {},
+                        'tanimoto': new_mol.get_tanimoto_similarity(),
+                        'drug_props': new_mol.drug_properties(),
+                        })
+    except:
+        return jsonify('failure')
+
+
+@app.route("/sketcher_choose", methods=['POST'])
+def sketcher_choose_molecule():
+    """Saves the final choice of molecule for the end of the game as a tuple of
+    the final molecule's R Group IDs.
+    Pass R group IDs as queries: /choose?r1=A01&r2=B10
+
+    :returns: Tuple of the R Group IDs as a json dict. Access tuple with
+    'chosen_mol' key.
+    :rtype: json dict
+    """
+    chosen_mol[0] = request.args.get('id')
+    chosen_mol[1] = request.args.get('smiles')
+    return jsonify({'chosen_mol': chosen_mol})
+
+
+@app.route("/sketcher_comparisontxt")
+def sketcher_comparison_txt():
+    """ Returns comparison text depending on pic50, logd, and
+    clearance_human of chosen molecule
+
+    returns: json dict with text in value depending on metric
+    rtype: json dict
+    """
+    assay_list = ['pic50', 'clearance_mouse', 'clearance_human',
+                  'logd', 'pampa']
+
+    mol = rdkit.Chem.MolFromSmiles(chosen_mol[1])
+    mol_block = rdkit.Chem.rdmolfiles.MolToMolBlock(mol)
+    drug_mol = sketchedMolecule(mol_block)
+    drug_properties = {
+        label: drug_mol.drug_properties()[label] for label in assay_list
+        }
+    # some properties for logd are strings ('best' or 'good') (no idea why)
+    drug_properties = numerise_params(drug_properties)
+
+    comp_dict = {}
+    with open('./src/comparison.txt', 'r') as f:
+        lines = f.readlines()
+        if float(drug_properties['pic50']) < 6.5:
+            comp_dict['pic50'] = str(lines[0])
+        else:
+            comp_dict['pic50'] = str(lines[1])
+        if float(drug_properties['logd']) < 0.95:
+            comp_dict['logd'] = str(lines[2])
+        elif 0.95 < float(drug_properties['logd']) < 1.15:
+            comp_dict['logd'] = str(lines[3])
+        else:
+            comp_dict['logd'] = str(lines[4])
+        if drug_properties['clearance_human'] != str(1):
+            comp_dict['clearance_human'] = str(lines[5])
+        else:
+            comp_dict['clearance_human'] = str(lines[6])
+    return jsonify({'comparison': comp_dict})
+
+
+@app.route("/sketcher_getspiderdata")
+def sketcher_return_spider_data():
+    """Takes final chosen molecule (chosen_mol) and calls FinalMolecule()
+     from Molecule.py to return quantitative assay parameters of the chosen
+     molecule and the reference drug
+     Calls /getspiderdata + FinalMolecule() + numerise_params()
+
+     :returns: A json dictionary containing a list of 2 dictionaries, one
+     containing chosen mol.parameters and the other containing reference
+     drug parameters
+     rtype: json dict
+     """
+
+    assay_list = ['pic50', 'clearance_mouse', 'clearance_human',
+                  'logd', 'pampa']
+    mol = rdkit.Chem.MolFromSmiles(chosen_mol[1])
+    mol_block = rdkit.Chem.rdmolfiles.MolToMolBlock(mol)
+    drug_mol = sketchedMolecule(mol_block)
+
+    drug_properties = {
+        label: drug_mol.drug_properties()[label] for label in assay_list
+        }
+    ref_mol = FinalMolecule('A05', 'B07')
+    ref_properties = {
+        label: ref_mol.drug_properties()[label] for label in assay_list
+        }
+    drug_properties = numerise_params(drug_properties)
+    ref_properties = numerise_params(ref_properties)
+    property_arr = [drug_properties, ref_properties]
+    return jsonify({'param_dict': property_arr})
 
 
 # e.g. http://127.0.0.1:5000/update_time_money
