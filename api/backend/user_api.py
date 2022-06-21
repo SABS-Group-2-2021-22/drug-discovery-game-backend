34b12bfc1d94afcbe0321aea7da2eaddc09a24ae
guidodee <guydurant@btinternet.com>
2022-06-21 12:27:02 +0100

    Added doc strings for user_api.py

--- api/backend/user_api.py
+++ api/backend/user_api.py
@@ -3,6 +3,12 @@ from src.user import User
 
 
 def authenticate_login():
+    """ Creates new instance of User object using user sent from the frontend
+    as API call.
+
+    :return: The request data and the new User object
+    :rtype: json dict, User object
+    """    
     request_data = request.get_json()
     user = User(request_data['username'])
     return jsonify(request_data), user
