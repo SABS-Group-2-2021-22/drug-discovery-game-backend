999c28bbafa656e74bb1553de65b768304c7e792
guidodee <guydurant@btinternet.com>
2022-09-26 10:22:37 +0100

    Added test for api.user aiuthenticate_login function

--- tests/test_api_backend_user_api.py
+++ tests/test_api_backend_user_api.py
@@ -1,3 +1,4 @@
+from re import A
 import unittest
 import api.backend as api
 from src.user import User
@@ -11,6 +12,8 @@ class TestUserAPI(unittest.TestCase):
         test_request_data, test_user = api.authenticate_login(test_data)
         self.assertEqual(test_request_data, test_data)
         true_user = User('User')
-        print(true_user)
-        print(test_user)
-        self.assertEqual(test_user, true_user)
+        self.assertEqual(test_user.chosen_mol, true_user.chosen_mol)
+        self.assertEqual(test_user.username, true_user.username)
+        self.assertEqual(test_user.money, true_user.money)
+        self.assertEqual(test_user.time, true_user.time)
+        self.assertEqual(test_user.molecule_info, true_user.molecule_info)
