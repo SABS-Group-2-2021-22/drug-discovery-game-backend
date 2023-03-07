6f63f44dca69c1a8b428ebfa9b842e363867f756
Nele Quast <nele.pauline@gmail.com>
2023-03-07 14:40:23 +0000

    #173 login logout and initial sesion loading code

--- api/backend/user_api.py
+++ api/backend/user_api.py
@@ -2,8 +2,8 @@ from src.user import User
 
 
 def authenticate_login(request_data):
-    """ Creates new instance of User object using username sent from the frontend
-    as API call.
+    """ Creates new instance of User object using username sent from the
+    frontend as API call.
 
     :return: The request data and the new User object
     :rtype: json dict, User object
